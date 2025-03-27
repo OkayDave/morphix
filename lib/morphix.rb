decee40bf56dc0da5926fdbce630c9dd6e961203
Dave Russell <dave.kerr@gmail.com>
2025-03-27 04:46:41 +0000

    Initial commit

---
+++ lib/morphix.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "morphix/version"
+
+module Morphix
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/morphix/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Morphix
+  VERSION = "0.1.0"
+end
